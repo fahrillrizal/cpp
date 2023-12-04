5cb322f5522c37c3329caa53f85cb26a470e523b
fahrillrizal <arilcoy14@gmail.com>
2023-12-04 21:39:17 +0700

    fixing bug 5.c

--- 5.c
+++ 5.c
@@ -12,112 +12,55 @@ struct Transaksi {
 
 struct Keuangan {
     double saldo;
+    struct Transaksi transaksi[100];
+    int jumlahTransaksi;
 };
 
-void bacaSaldoDariFile(struct Keuangan *keuangan) {
-    FILE *file = fopen("transaksi.txt", "r");
-    if (file != NULL) {
-        struct Transaksi transaksi;
-        while (fscanf(file, "%s %s %[^\n] %lf", transaksi.tanggal, transaksi.jenis, transaksi.keterangan, &transaksi.jumlah) == 4) {
-            if (strcmp(transaksi.jenis, "Pendapatan") == 0 || strcmp(transaksi.jenis, "p") == 0) {
-                keuangan->saldo += transaksi.jumlah;
-            } else if (strcmp(transaksi.jenis, "Pengeluaran") == 0 || strcmp(transaksi.jenis, "k") == 0) {
-                keuangan->saldo -= transaksi.jumlah;
-            }
-        }
-        fclose(file);
-    } else {
-        fprintf(stderr, "Gagal membuka file transaksi.txt\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void simpanSaldoKeFile(struct Keuangan *keuangan) {
-    FILE *file = fopen("transaksi.txt", "a");
-    if (file != NULL) {
-        time_t now = time(NULL);
-        struct tm *ltm = localtime(&now);
-        fprintf(file, "%02d/%02d/%d Saldo %.2lf\n", ltm->tm_mday, 1 + ltm->tm_mon, 1900 + ltm->tm_year, keuangan->saldo);
-        fclose(file);
-    } else {
-        fprintf(stderr, "Gagal membuka file transaksi.txt\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void simpanTransaksi(const char *tanggal, const char *jenis, const char *keterangan, double jumlah) {
-    struct Transaksi transaksi;
-    strcpy(transaksi.tanggal, tanggal);
-    strcpy(transaksi.jenis, jenis);
-    strcpy(transaksi.keterangan, keterangan);
-    transaksi.jumlah = jumlah;
-
-    FILE *file = fopen("transaksi.txt", "a");
-    if (file != NULL) {
-        fprintf(file, "%s %s %s %.2lf\n", transaksi.tanggal, transaksi.jenis, transaksi.keterangan, transaksi.jumlah);
-        fclose(file);
-    } else {
-        fprintf(stderr, "Gagal membuka file transaksi.txt\n");
-        exit(EXIT_FAILURE);
-    }
+void getCurrentDate(char *date) {
+    time_t now = time(NULL);
+    struct tm *ltm = localtime(&now);
+    sprintf(date, "%02d/%02d/%d", ltm->tm_mday, 1 + ltm->tm_mon, 1900 + ltm->tm_year);
 }
 
 void tampilkanLaporan(struct Keuangan *keuangan) {
-    FILE *file = fopen("transaksi.txt", "r");
-    if (file != NULL) {
-        printf("=========== LAPORAN KEUANGAN ===========\n");
-        printf("Saldo saat ini: %.2lf\n", keuangan->saldo);
-        printf("---------------------------------------\n");
-        printf("%-15s%-15s%-25s%-15s\n", "Tanggal", "Jenis", "Keterangan", "Jumlah");
-        printf("---------------------------------------\n");
+    printf("=========== LAPORAN KEUANGAN ===========\n");
+    printf("Saldo saat ini: %.2lf\n", keuangan->saldo);
+    printf("---------------------------------------\n");
+    printf("%-15s%-15s%-25s%-15s\n", "Tanggal", "Jenis", "Keterangan", "Jumlah");
+    printf("---------------------------------------\n");
 
-        struct Transaksi transaksi;
-
-        while (fscanf(file, "%s %s %[^\n] %lf", transaksi.tanggal, transaksi.jenis, transaksi.keterangan, &transaksi.jumlah) == 4) {
-            printf("%-15s%-15s%-25s%.2lf\n", transaksi.tanggal, transaksi.jenis, transaksi.keterangan, transaksi.jumlah);
-        }
-
-        printf("=======================================\n");
-        fclose(file);
-    } else {
-        fprintf(stderr, "Gagal membuka file transaksi.txt\n");
-        exit(EXIT_FAILURE);
+    for (int i = 0; i < keuangan->jumlahTransaksi; ++i) {
+        struct Transaksi transaksi = keuangan->transaksi[i];
+        printf("%-15s%-15s%-25s%.2lf\n", transaksi.tanggal, transaksi.jenis, transaksi.keterangan, transaksi.jumlah);
     }
+
+    printf("=======================================\n");
 }
 
-char *getCurrentDate() {
-    time_t now = time(NULL);
-    struct tm *ltm = localtime(&now);
-
-    char *date = (char *)malloc(11 * sizeof(char));
-    sprintf(date, "%02d/%02d/%d", ltm->tm_mday, 1 + ltm->tm_mon, 1900 + ltm->tm_year);
-
-    return date;
-}
-
-void catatPendapatan(struct Keuangan *keuangan) {
-    double jumlah;
+void catatPendapatan(struct Keuangan *keuangan, double jumlah) {
     keuangan->saldo += jumlah;
-
-    char *tanggal = getCurrentDate();
 
     char keterangan[100];
     printf("Masukkan keterangan pendapatan: ");
-    getchar(); // Consume newline character left in the buffer
+    getchar();
     fgets(keterangan, sizeof(keterangan), stdin);
-    keterangan[strcspn(keterangan, "\n")] = '\0'; // Remove trailing newline character
-
-    simpanTransaksi(tanggal, "Pendapatan", keterangan, jumlah);
-    free(tanggal);
-}
-
-void catatPengeluaran(struct Keuangan *keuangan) {
-    double jumlah;
-    keuangan->saldo -= jumlah;
+    keterangan[strcspn(keterangan, "\n")] = '\0';
 
     char tanggal[11];
-    printf("Masukkan tanggal pengeluaran (dd/mm/yyyy): ");
+    printf("Masukkan tanggal (dd/mm/yyyy): ");
     scanf("%s", tanggal);
+
+    struct Transaksi transaksi;
+    strcpy(transaksi.tanggal, tanggal);
+    strcpy(transaksi.jenis, "Pendapatan");
+    strcpy(transaksi.keterangan, keterangan);
+    transaksi.jumlah = jumlah;
+
+    keuangan->transaksi[keuangan->jumlahTransaksi++] = transaksi;
+}
+
+void catatPengeluaran(struct Keuangan *keuangan, double jumlah) {
+    keuangan->saldo -= jumlah;
 
     char keterangan[100];
     printf("Masukkan keterangan pengeluaran: ");
@@ -125,14 +68,23 @@ void catatPengeluaran(struct Keuangan *keuangan) {
     fgets(keterangan, sizeof(keterangan), stdin);
     keterangan[strcspn(keterangan, "\n")] = '\0';
 
-    simpanTransaksi(tanggal, "Pengeluaran", keterangan, jumlah);
+    char tanggal[11];
+    printf("Masukkan tanggal (dd/mm/yyyy): ");
+    scanf("%s", tanggal);
+
+    struct Transaksi transaksi;
+    strcpy(transaksi.tanggal, tanggal);
+    strcpy(transaksi.jenis, "Pengeluaran");
+    strcpy(transaksi.keterangan, keterangan);
+    transaksi.jumlah = jumlah;
+
+    keuangan->transaksi[keuangan->jumlahTransaksi++] = transaksi;
 }
 
 int main() {
     struct Keuangan keuangan;
     keuangan.saldo = 0.0;
-
-    bacaSaldoDariFile(&keuangan);
+    keuangan.jumlahTransaksi = 0;
 
     char pilihan;
     double jumlah;
@@ -146,14 +98,14 @@ int main() {
             case 'p':
                 printf("Masukkan jumlah pendapatan (dalam jutaan): ");
                 scanf("%lf", &jumlah);
-                catatPendapatan(&keuangan);
+                catatPendapatan(&keuangan, jumlah);
                 break;
 
             case 'K':
             case 'k':
                 printf("Masukkan jumlah pengeluaran: ");
                 scanf("%lf", &jumlah);
-                catatPengeluaran(&keuangan);
+                catatPengeluaran(&keuangan, jumlah);
                 break;
 
             case 'L':
@@ -171,8 +123,6 @@ int main() {
         }
 
     } while (pilihan != 'S' && pilihan != 's');
-
-    simpanSaldoKeFile(&keuangan);
 
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+// Remove trailing newline character
